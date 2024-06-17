d644e596655a3ce8cbf206b9535b487d3e0fabae
Kauê Cavalcante <kaue_cavalcante.cnt@outlook.com>
2024-06-17 12:24:53 -0300

    Created header component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,21 @@
 import { globalStyles } from "@/styles/global";
 import type { AppProps } from "next/app";
 import { Roboto } from "next/font/google";
+import logo from '../assets/logo.svg'
+import { Container, Header } from "@/styles/pages/app";
+import Image from "next/image";
 
 const roboto = Roboto({ weight: ["400", "700"], subsets: ["latin"] });
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} className={roboto.className} />;
+  return (
+    <Container>
+      <Header>
+        <Image src={logo} alt="" />
+      </Header>
+      <Component {...pageProps} className={roboto.className} />
+    </Container>
+  );
 }
