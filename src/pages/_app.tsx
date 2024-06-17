998fb9f07bcdb5a96324702f45566fa2dff765ee
Kauê Cavalcante <kaue_cavalcante.cnt@outlook.com>
2024-06-17 11:37:21 -0300

    Project setup and styles configured

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import "@/styles/globals.css";
+import { globalStyles } from "@/styles/global";
 import type { AppProps } from "next/app";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({ weight: ["400", "700"], subsets: ["latin"] });
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} className={roboto.className} />;
 }
