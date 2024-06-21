d3f705f17e7faa9e7e8bcc5e6f7aecbf065fef70
Kauê Cavalcante <kaue_cavalcante.cnt@outlook.com>
2024-06-21 17:56:18 -0300

    Integrated stripe api

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+
+    remotePatterns: [
+      {
+        hostname: 'files.stripe.com',
+        protocol: 'https'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
