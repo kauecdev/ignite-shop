28d93b695989b15b4c2c4788409860470d4eab9c
Kauê Cavalcante <kaue_cavalcante.cnt@outlook.com>
2024-06-21 18:07:21 -0300

    Changed home page to be static

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,11 @@
 import Image from "next/image";
 import { useKeenSlider } from 'keen-slider/react'
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Stripe from "stripe";
 
 import 'keen-slider/keen-slider.min.css'
 
 import { HomeContainer, Product } from "@/styles/pages/home";
-import camiseta1 from '@/assets/camisetas/1.png'
-import camiseta2 from '@/assets/camisetas/2.png'
-import camiseta3 from '@/assets/camisetas/3.png'
 import { stripe } from "@/lib/stripe";
 
 interface HomeProps {
@@ -48,7 +45,7 @@ export default function Home({ products }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   })
